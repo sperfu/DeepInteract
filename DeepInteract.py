bb2f60b9c0dd16cc83c82147668ed19d4f593501
sperfu <fulaiyi321@163.com>
2017-03-27 17:13:28 +0800

    main code

--- DeepInteract.py
+++ DeepInteract.py
@@ -42,6 +42,8 @@ from keras import regularizers
 from keras.constraints import maxnorm
 from keras.optimizers import kl_divergence
 
+from DeepFunction import multiple_layer_autoencoder,autoencoder_two_subnetwork_fine_tuning
+
 def prepare_data(seperate=False):
     print "loading data"
     #miRNA_fea = np.loadtxt("circRNA_functional_sim.txt",dtype=float,delimiter=",")
@@ -164,132 +166,7 @@ def DNN2():
     model.compile(loss='binary_crossentropy', optimizer=adadelta, class_mode="binary")##rmsprop sgd
     return model
 
-def multiple_layer_autoencoder(X_train, X_test, activation = 'linear', batch_size = 100, nb_epoch = 100, last_dim = 64):
-    nb_hidden_layers = [X_train.shape[1], 256, 128, last_dim]
-    X_train_tmp = np.copy(X_train)
-    #X_test_tmp = np.copy(X_test)
-    encoders = []
-    autoencoders = []
-    for i, (n_in, n_out) in enumerate(zip(nb_hidden_layers[:-1], nb_hidden_layers[1:]), start=1):
-        print('Training the layer {}: Input {} -> Output {}'.format(i, n_in, n_out))
-        # Create AE and training
-        #ae = Sequential()
-	input_img = Input(shape=(n_in,))
-        #encoder = containers.Sequential([Dense(input_dim=n_in, output_dim=n_out, activation=activation)])
-        encode = Dense(output_dim=n_out, activation=activation)(input_img)
-        #decoder = containers.Sequential([Dense(input_dim=n_out,output_dim=n_in, activation=activation)]) #, activation=activation)])
-        decode = Dense(output_dim=n_in, activation=activation)(encode) #, activation=activation)])
-        #ae.add(AutoEncoder(encoder=encoder, decoder=decoder,
-        #                   output_reconstruction=True))
-        #ae1 = AutoEncoder(encoder=encoder, decoder=decoder,output_reconstruction=True)
-	autoencoder = Model(input=input_img, output=decode)
-	encoder = Model(input=input_img, output=encode)
-	encoded_input = Input(shape=(n_out,))
-	decoder_layer = autoencoder.layers[-1]
-	decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
-	autoencoder.compile(loss='mean_squared_error', optimizer='adam')
-	autoencoder.fit(X_train_tmp, X_train_tmp,nb_epoch=10,batch_size=100 ,shuffle=True,validation_data=None, verbose=0)
-	encoder.compile(loss='mean_squared_error', optimizer='adam')
-	#ae.add(encoder)
-	#ae.add(decoder)
-	#ae.add(ae1)
-        #ae.add(Dropout(0.5))
-        #sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-        #ae.compile(loss='mean_squared_error', optimizer='adam')#  adam  'rmsprop')
-        #ae.fit(X_train_tmp, X_train_tmp, batch_size=batch_size, nb_epoch=nb_epoch, validation_data=None,show_accuracy=True, verbose=1)
-        # Store trainined weight and update training data
-        #encoders.append(ae.layers[0].encoder)
-        encoders.append(encoder)
-	autoencoders.append(autoencoder)
-	#ae1.output_reconstruction= False
-        X_train_tmp = encoder.predict(X_train_tmp,batch_size = 100)
-        print X_train_tmp.shape
-        #X_test_tmp = ae.predict(X_test_tmp)
-        
-    #return encoders, X_train_tmp, X_test_tmp
-    return encoders,autoencoders
 
-def autoencoder_two_subnetwork_fine_tuning(X_train1, X_train2, Y_train, X_test1, X_test2, Y_test = None, batch_size =100, nb_epoch = 10):
-    print 'autoencode learning'
-    last_dim = 64
-    encoders1, autoencoders1 = multiple_layer_autoencoder(X_train1, X_test1, activation = 'sigmoid', batch_size = batch_size, nb_epoch = nb_epoch, last_dim = last_dim)
-    encoders2, autoencoders2 = multiple_layer_autoencoder(X_train2, X_test2, activation = 'sigmoid', batch_size = batch_size, nb_epoch = nb_epoch, last_dim = last_dim)
-    #pdb.set_trace()
-    
-    X_train1_tmp_bef = np.copy(X_train1)
-    X_test1_tmp_bef = np.copy(X_test1) 
-    for ae in encoders1:
-        X_train1_tmp_bef = ae.predict(X_train1_tmp_bef)
-        print X_train1_tmp_bef.shape
-        X_test1_tmp_bef = ae.predict(X_test1_tmp_bef)
-    
-    X_train2_tmp_bef = np.copy(X_train2)
-    X_test2_tmp_bef = np.copy(X_test2) 
-    for ae in encoders2:
-        X_train2_tmp_bef = ae.predict(X_train2_tmp_bef)
-        print X_train2_tmp_bef.shape
-        X_test2_tmp_bef = ae.predict(X_test2_tmp_bef)
-        
-    prefilter_train_bef = np.concatenate((X_train1_tmp_bef, X_train2_tmp_bef), axis = 1)
-    prefilter_test_bef = np.concatenate((X_test1_tmp_bef, X_test2_tmp_bef), axis = 1)
-        
-    print 'fine tunning'
-    print 'number of layers:', len(autoencoders1)
-    sec_num_hidden = last_dim
-    model1 = Sequential()
-    ind = 0
-    for encoder in autoencoders1:
-        model1.add(encoder.layers[1])
-        if ind != len(autoencoders1)  - 1 :
-            model1.add(Dropout(0.5)) 
-            ind = ind + 1
-    model1.add(PReLU())
-    model1.add(BatchNormalization((sec_num_hidden,)))
-    model1.add(Dropout(0.5))
-    
-
-    model2 = Sequential()
-    ind = 0
-    for encoder in autoencoders2:
-        model2.add(encoder.layers[1])
-        if ind != len(autoencoders2)  - 1 :
-            model2.add(Dropout(0.5)) 
-            ind = ind + 1
-    model2.add(PReLU())
-    model2.add(BatchNormalization((sec_num_hidden,)))   
-    model2.add(Dropout(0.5))     
-         
-    model = Sequential()
-    model.add(Merge([model1, model2], mode='concat'))
-    total_hid = sec_num_hidden + sec_num_hidden
-    
-    model.add(Dense(input_dim=total_hid,output_dim= 2))
-    model.add(Dropout(0.5))
-    model.add(Activation('softmax'))
-    #model.get_config(verbose=0)
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd) #'rmsprop')
-    model.fit([X_train1, X_train2], Y_train, batch_size=100, nb_epoch=15, verbose=0)
-    #config = autoencoder.get_config(verbose=1)
-    #autoencoder = model_from_config(config)
-    X_train1_tmp = np.copy(X_train1)
-    X_test1_tmp = np.copy(X_test1)  
-    ae=model.layers[0].layers[0]  
-    ae.compile(loss='mean_squared_error', optimizer='adam')
-    X_train1_tmp = ae.predict(X_train1_tmp)
-    X_test1_tmp = ae.predict(X_test1_tmp)
-
-    X_train2_tmp = np.copy(X_train2)
-    X_test2_tmp = np.copy(X_test2)  
-    ae=model.layers[0].layers[1]  
-    ae.compile(loss='mean_squared_error', optimizer='adam')
-    X_train2_tmp = ae.predict(X_train2_tmp)
-    X_test2_tmp = ae.predict(X_test2_tmp)
-    
-    prefilter_train = np.concatenate((X_train1_tmp, X_train2_tmp), axis = 1)
-    prefilter_test = np.concatenate((X_test1_tmp, X_test2_tmp), axis = 1)
-    #return X_train1_tmp, X_test1_tmp, X_train2_tmp, X_test2_tmp, model
-    return prefilter_train, prefilter_test, prefilter_train_bef, prefilter_test_bef
 
 def DeepInteract():
     X, labels = prepare_data(seperate = True)
